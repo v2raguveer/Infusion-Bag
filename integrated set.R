967fce0456481224b2a13d834ae5455f518b0a86
Vanitha Raguveer <vanitha.raguveer@gmail.com>
2019-05-18 15:42:51 -0400

    Update integrated set.R

--- integrated set.R
+++ integrated set.R
@@ -1,7 +1,7 @@
 library(Seurat)
 library(cowplot)
-ctrl.data <- Read10X(data.dir = "C:/Users/raguveerv2/Documents/Data/Infusion Bag/cellrangerFiltered/s284_filterMatrix")
-acti.data <- Read10X(data.dir = "C:/Users/raguveerv2/Documents/Data/Infusion Bag/cellrangerFiltered/s285_filterMatrix")
+ctrl.data <- Read10X(data.dir = "/Users/vanitharaguveer/Documents/RData/cellrangerFiltered/s284_filterMatrix")
+acti.data <- Read10X(data.dir = "/Users/vanitharaguveer/Documents/RData/cellrangerFiltered/s285_filterMatrix")
 
 ctrl <- CreateSeuratObject(counts = ctrl.data, project = "Immune_ctrl", min.cells = 5)
 ctrl$stim <- "CTRL"
@@ -21,13 +21,18 @@ immune.combined <- IntegrateData(anchorset = immune.anchors, dims = 1:20)
 DefaultAssay(immune.combined) <- "integrated"
 immune.combined <- ScaleData(immune.combined, verbose=FALSE)
 immune.combined <- RunPCA(immune.combined, npcs = 30, verbose = FALSE)
+immune.combined <- RunUMAP(immune.combined, reduction = 'pca', dims = 1:20)
 immune.combined <- RunTSNE(immune.combined, reduction = 'pca', dims = 1:20)
 immune.combined <- FindNeighbors(immune.combined, reduction = 'pca', dims=1:20)
 immune.combined <- FindClusters(immune.combined, resolution = .5)
-p1 <- DimPlot(immune.combined, reduction = 'tsne', group.by = 'stim')
-p2 <- DimPlot(immune.combined, reduction = 'tsne', label = TRUE)
-plot_grid(p1, p2)
-
+p1 <- DimPlot(immune.combined, reduction = 'umap', group.by = 'stim')
+p2 <- DimPlot(immune.combined, reduction = 'umap', label = TRUE)
+p3 <- DimPlot(immune.combined, reduction = 'tsne', group.by = 'stim')
+p4 <- DimPlot(immune.combined, reduction = 'tsne', label = TRUE)
+plot(p1)
+plot(p2)
+plot(p3)
+plot(p4)
 DimPlot(immune.combined, reduction = 'tsne', split.by = "stim")
 
 DefaultAssay(immune.combined)
